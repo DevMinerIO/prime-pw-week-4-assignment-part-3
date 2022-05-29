0bf227f55866cb47e8a750b1ef9325e274762ac0
Tim Miner <poeminminer1@gmail.com>
2022-05-29 14:08:13 -0500

    finished part 3. Stretch goals WIP

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,32 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item) {
+    basket.push(item);
+    return true;
+}
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+function listItems() {
+    for( let i = 0; i < basket.length; i++) {
+        console.log(basket[i]);
+    }
+}
+listItems();
+//another add
+console.log("adding Doughnut");
+addItem("doughnut");
+listItems();
+
+function empty() {
+    basket = [];
+}
+
+console.log("items in the array");
+empty();
+console.log(`Basket is now ${basket}`);
